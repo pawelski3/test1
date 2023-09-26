34746c0c5bb8bee49275f513e702719caa049ac9
Pawel64342 <wsbp64342>
2023-09-26 20:26:24 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,49 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { WarningComponent } from './warning/warning.component';
+
+
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+export const appRouters: Routes = [
+  { path: '', component: OneComponent },
+  { path: 'one', component: OneComponent },
+  { path: 'two', component: TwoComponent },
+  // {
+  //   path: 'three', component: ThreeComponent,
+  //   data: [{ version: '1.1.8' }]
+  // },
+  // { path: 'four/:idOffer', component: FourComponent },
+  { path: '**', component: WarningComponent },
+
+
+]
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent,
+    WarningComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRouters),
   ],
   providers: [],
   bootstrap: [AppComponent]
